7b47eb5920f27f94ff1ff15c83fa3ff217d89945
thattal <thattal@f6r7s14.paris.42.school>
2026-07-28 20:25:41 +0200

    save

--- ex0/lambda_spells.py
+++ ex0/lambda_spells.py
@@ -1,6 +1,12 @@
 def artifact_sorter(artifacts: list[dict]) -> list[dict]:
     return sorted(artifacts, key=lambda a: a["power"], reverse=True)
 
+def power_filter(mages: list[dict], min_power: int) -> list[dict]:
+    return list(filter(lambda x: x["power"] >= min_power, mages))
+
+def spell_transformer(spells: list[str]) -> list[str]:
+    return list(map(lambda m: f"* {m} *", spells))
+
 artifacts: list[dict] = [
     {"name": "Sword of Flames", "power": 75, "type": "weapon"},
     {"name": "Amulet of Wisdom", "power": 40, "type": "accessory"},
@@ -8,4 +14,6 @@ artifacts: list[dict] = [
     {"name": "Ring of Shadows", "power": 60, "type": "accessory"},
 ]
 
-print(artifact_sorter(artifacts))
+# print(artifact_sorter(artifacts))
+# print(power_filter(artifacts, 80))
+# print(spell_transformer(("sdsds", "gdhged", "rt ywttw", "h")))
